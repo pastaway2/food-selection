bfd7400e176323205c554cfda7de60e4137a4a15
JB <jb7045955@gmail.com>
2022-10-11 15:51:55 +1100

    update action

--- index.js
+++ index.js
@@ -1,63 +1,75 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require("@octokit/core");
-//import { Octokit } from "octokit";
 
-// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-
-// Octokit.js
-// https://github.com/octokit/core.js#readme
 const octokit = new Octokit({
   auth: process.env.TOKEN
-  });
-  
+});
+
+let key = core.getInput('food_delete');
+if (key == 'food') {
+
+
+
+
+
 
   (async () => {
     try {
-        const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
-            owner: 'pastaway2',
-            repo: 'try-action'
-        });
+      const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+        owner: 'pastaway2',
+        repo: 'try-action'
+      });
 
 
-        const issues = response.data.map(r => {
-            return {
-                number: r.number,
-                title: r.title,
-                labels: r.labels.map(l => { return { name: l.name }})
-            }
-        })
-
-        const filterissues = issues.filter(issue => {
-          let haslabel = false
-
-          for(let label of issue.labels){
-            if(label.name == 'eat this week') haslabel= true
-          }
-          return !haslabel
+      const issues = response.data.map(r => {
+        return {
+          number: r.number,
+          title: r.title,
+          labels: r.labels.map(l => { return { name: l.name } })
+        }
       })
 
-        console.log('Repo issues ==> ', filterissues)
-        
+      const filterissues = issues.filter(issue => {
+        let haslabel = false
 
-        const random = Math.floor(Math.random() * filterissues.length);
+        for (let label of issue.labels) {
+          if (label.name == 'eat this week') haslabel = true
+        }
+        return !haslabel
+      })
 
-        const selected = filterissues[random]
-        console.log('This week I eat ==> ', selected)
+      console.log('Repo issues ==> ', filterissues)
 
-        if(selected == undefined)
-          return false
 
-        await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
+      const random = Math.floor(Math.random() * filterissues.length);
+
+      const selected = filterissues[random]
+      console.log('This week I eat ==> ', selected)
+
+      if (selected == undefined)
+        return false
+
+      await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
         owner: 'pastaway2',
         repo: 'try-action',
         issue_number: selected.number,
         labels: [
-        'eat this week', 
+          'eat this week',
         ]
-        })
+      })
 
     } catch (error) {
-        console.log('error  ==> ', error);
+      console.log('error  ==> ', error);
     }
-})();
+  })();
+
+} else {
+  (async () => {
+    octokit.request('DELETE /repos/{owner}/{repo}/labels/{name}', {
+      owner: 'pastaway2',
+      repo: 'try-action',
+      name: 'eat this week'
+    })
+  })();
+}
